5701f8d092ea668f7647b194d99c2b2ea54c0d0d
Fredrik Sigvartsen <fredrik@sealab.no>
2020-02-06 15:15:40 +0100

    Styling sealab-world

--- src/pages/sealab-world.js
+++ src/pages/sealab-world.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Livestream from '../components/Livestream';
+import Title from '../components/Title';
 
 const World = () => (
   <Layout>
-    <section className="section is-medium blog-content">
-      <div className="container content">
-        <h1>SEALAB WORLD</h1>
+    <section className="section blog-content has-dark-background">
+      <div
+        className="container content"
+        style={{ width: '60vw', margin: 'auto' }}
+      >
+        <Title
+          title="SEALAB WORLD"
+          description="Sjekk ut Livestream fra vårt Demo-anlegg"
+          position="center"
+        />
         <Livestream />
       </div>
     </section>
